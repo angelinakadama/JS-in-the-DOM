31dd99a61297059d3a09de679b5e8651f63224f2
angelinakadama <angelinakadama32@gmail.com>
2019-06-21 05:53:28 -0700

    change events html& js

--- events.js
+++ events.js
@@ -1,6 +1,12 @@
-// Function to modify the text content of the paragraph
-const changeText = () => {
-    const p = document.querySelector('p');
+const p = document.querySelector('p');
+const button = document.querySelector('button');
 
-    p.textContent = "I changed because of an inline event handler.";
+const changeText = () => {
+    p.textContent = "Will I change?";
 }
+const alertText = () => {
+    alert('Will I alert?');
+}
+// Events can be overwritten
+button.onclick = changeText;
+button.onclick = alertText;
